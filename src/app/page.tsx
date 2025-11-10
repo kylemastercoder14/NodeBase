d1afe8794d39055e301ddcf96ee3c056508a6d3c
jeffreelingal011 <jeffreelingal00@gmail.com>
2025-11-10 08:25:51 +0800

    03: install inngest-dev

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,34 @@
 "use client";
 
-import { authClient } from '@/lib/auth-client';
+import { authClient } from "@/lib/auth-client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useTRPC } from "@/trpc/client";
+import { Button } from '../components/ui/button';
 
 const Page = () => {
-  const {data} = authClient.useSession();
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        queryClient.invalidateQueries(trpc.getWorkflows.queryOptions());
+      },
+    })
+  );
   return (
     <div>
       {JSON.stringify(data)}
       {data && (
-        <button className='bg-destructive' onClick={() => authClient.signOut()}>Logout</button>
+        <button className="bg-destructive" onClick={() => authClient.signOut()}>
+          Logout
+        </button>
       )}
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>
+        Create workflow
+      </Button>
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
